42a05226a68880645a28cd63499f57e087c8ac9c
kitkovsky <okitkowski114@gmail.com>
2022-02-03 18:16:17 +0100

    add contrast checking to text and icons

--- app.js
+++ app.js
@@ -6,9 +6,26 @@ const currentHexes = document.querySelectorAll(".color h2");
 function randomColours() {
     colourDivs.forEach((div) => {
         const hexText = div.children[0];
-        const randomColour = chroma.random().hex();
-        div.style.backgroundColor = randomColour;
-        hexText.innerText = randomColour;
+        const randomColour = chroma.random();
+        const randomColourHex = randomColour.hex();
+        div.style.backgroundColor = randomColourHex;
+        hexText.innerText = randomColourHex;
+        checkTextContrast(randomColour, div);
     });
+}
+function checkTextContrast(colour, colourDiv) {
+    const text = colourDiv.children[0];
+    const sliders = colourDiv.children[1].children[0];
+    const lock = colourDiv.children[1].children[1];
+    if (colour.luminance() > 0.5) {
+        text.style.color = "black";
+        sliders.style.color = "black";
+        lock.style.color = "black";
+    }
+    else {
+        text.style.color = "white";
+        sliders.style.color = "white";
+        lock.style.color = "white";
+    }
 }
 randomColours();
--- app.ts
+++ app.ts
@@ -5,14 +5,30 @@ const sliders = document.querySelectorAll<HTMLInputElement>(
 );
 const currentHexes = document.querySelectorAll<HTMLDivElement>(".color h2");
 
-function randomColours(){
+function randomColours() {
   colourDivs.forEach((div) => {
     const hexText = div.children[0] as HTMLElement;
-    const randomColour = chroma.random().hex();
+    const randomColour = chroma.random();
+    const randomColourHex = randomColour.hex();
 
-    div.style.backgroundColor = randomColour;
-    hexText.innerText = randomColour;
+    div.style.backgroundColor = randomColourHex;
+    hexText.innerText = randomColourHex;
+    checkTextContrast(randomColour, div);
   });
 }
 
+function checkTextContrast(colour: chroma.Color, colourDiv: HTMLDivElement) {
+  const text = colourDiv.children[0] as HTMLElement;
+  const sliders = colourDiv.children[1].children[0] as HTMLElement;
+  const lock = colourDiv.children[1].children[1] as HTMLElement;
+  if (colour.luminance() > 0.5) {
+    text.style.color = "black";
+    sliders.style.color = "black";
+    lock.style.color = "black";
+  } else {
+    text.style.color = "white";
+    sliders.style.color = "white";
+    lock.style.color = "white";
+  }
+}
 randomColours();
